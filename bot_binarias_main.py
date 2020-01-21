36598e10c6143a2af47f344e867088d36b588428
thehaseo <gregoriazuaje04@gmail.com>
2020-01-20 23:18:53 -0400

    - añadido codigo para tomar en cuenta las noticias fundamentales - añadidas imagenes para la ejecucion de la compra y la venta

--- bot_binarias_main.py
+++ bot_binarias_main.py
@@ -1,6 +1,4 @@
-from ExtraccionDatosFxcmpy import ExtraccionFxcmpy
 from ExtraccionDatosOanda import ExtraccionOanda
-from distancia_a_soporte_y_resistencia import distancias
 from ADX import ADX
 from RSI import RSI
 import oandapyV20
@@ -44,21 +42,19 @@ def ejecutar():
     time.sleep(0.1)
     click_image("1minuto.jpg", (1517, 220), "left", 0.05)
     click_image("imagen compra.jpg", (1801, 379), "left", 0.05)
-    time.sleep(0.05)
-    click_image("imagen venta.jpg", (1804, 513), "left", 0.05)
 
 
-def run(tiempo_de_ejecucion_minutos, primera_divisa, segunda_divisa):
+def run(tiempo_de_ejecucion_minutos, primera_divisa, segunda_divisa, numero_noticias, horas_noticias):
     print("comenzando")
-    datos_operaciones = pd.Dataframe()
+    datos_operaciones = pd.DataFrame(columns=("precio", "DI+"))
     timeout = time.time() + (tiempo_de_ejecucion_minutos * 60)
     divisa = f"{primera_divisa}_{segunda_divisa}"
-    proceso_1_min = ExtraccionFxcmpy(500, "m1", f"{primera_divisa}/{segunda_divisa}")
+    proceso_1_min = ExtraccionOanda(500, "M1", f"{primera_divisa}_{segunda_divisa}")
     proceso_5_min = ExtraccionOanda(120, "M5", f"{primera_divisa}_{segunda_divisa}")
     proceso_1_min.start()
     proceso_5_min.start()
     time.sleep(30)
-    datos_1min = pd.read_csv("datos_m1.csv", index_col="date")
+    datos_1min = pd.read_csv("datos_M1.csv", index_col="time")
     resistencia_mayor_1m = datos_1min["h"].rolling(150).max().dropna()
     resistencia_menor_1m = datos_1min["c"].rolling(150).max().dropna()
     resistencia_punto_mayor_1m = resistencia_mayor_1m.iloc[-1]
@@ -133,10 +129,26 @@ def run(tiempo_de_ejecucion_minutos, primera_divisa, segunda_divisa):
     live_price_request = pricing.PricingInfo(accountID=account_id, params={"instruments": divisa})
     rango_precios = []
     while time.time() <= timeout:
-        try:
-            if f"{(int(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))[14:16]) - 1):02}" != \
-                    datos_1min.iloc[-1].name[14:16]:
-                datos_1min = pd.read_csv("datos_m1.csv", index_col="date")
+            if numero_noticias == 1:
+                if time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))[:-3] == horas_noticias[0]:
+                    time.sleep(3600)
+            elif numero_noticias == 2:
+                if time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))[:-3] == horas_noticias[0]:
+                    time.sleep(3600)
+                elif time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))[:-3] == horas_noticias[1]:
+                    time.sleep(3600)
+            elif numero_noticias == 3:
+                if time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))[:-3] == horas_noticias[0]:
+                    time.sleep(3600)
+                elif time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))[:-3] == horas_noticias[1]:
+                    time.sleep(3600)
+                elif time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))[:-3] == horas_noticias[2]:
+                    time.sleep(3600)
+            if (f"{(int(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))[14:16]) - 1):02}" != \
+                datos_1min.iloc[-1].name[14:16]) and \
+                    (f"{(int(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))[14:16])):02}" != \
+                     datos_1min.iloc[-1].name[14:16]):
+                datos_1min = pd.read_csv("datos_M1.csv", index_col="time")
                 resistencia_mayor_1m = datos_1min["h"].rolling(150).max().dropna()
                 resistencia_menor_1m = datos_1min["c"].rolling(150).max().dropna()
                 resistencia_punto_mayor_1m = resistencia_mayor_1m.iloc[-1]
@@ -166,7 +178,7 @@ def run(tiempo_de_ejecucion_minutos, primera_divisa, segunda_divisa):
             if ((int(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))[15:16])) == 1 or (
                     int(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))[15:16])) == 6) and \
                     (datos_5min.iloc[-1].name[
-                     14:16] != f"{int(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))[14:16]) - 1}"):
+                     14:16] != f"{int(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))[14:16]) - 1:02}"):
                 datos_5min = pd.read_csv("datos_M5.csv", index_col="time")
                 resistencia_mayor_5m = datos_5min["h"].rolling(50).max().dropna()
                 resistencia_menor_5m = datos_5min["c"].rolling(50).max().dropna()
@@ -213,6 +225,7 @@ def run(tiempo_de_ejecucion_minutos, primera_divisa, segunda_divisa):
             last_data_row['c'] = round(rango_precios[-1], 6)
             datos_5s = datos_5s.append(last_data_row, sort=False)
             datos_5s = datos_5s.iloc[-500:]
+            datos_5s.index.name = "time"
             ejecutar()
             adx_5min = ADX(datos_5min, 14)
             adx_1min = ADX(datos_1min, 21)
@@ -231,6 +244,7 @@ def run(tiempo_de_ejecucion_minutos, primera_divisa, segunda_divisa):
             corte_bajista_adx = "Y" if (adx_5s["DI-"].iloc[-2] < adx_5s["DI+"].iloc[-2] and
                                         adx_5s["DI-"].iloc[-1] > adx_5s["DI+"].iloc[-1]) else "N"
             ganada_perdida = input("fue ganadora?: ")
+            datos_operaciones["precio"] += datos_5s['c'].iloc[-1]
             datos_operaciones["ADX 5min"] += adx_5min["ADX"].iloc[-1]
             datos_operaciones["DI- 5min"] += adx_5min["DI-"].iloc[-1]
             datos_operaciones["DI+ 5min"] += adx_5min["DI+"].iloc[-1]
@@ -245,10 +259,9 @@ def run(tiempo_de_ejecucion_minutos, primera_divisa, segunda_divisa):
             datos_operaciones["corte alcista"] += corte_alcista_adx
             datos_operaciones["corte bajista"] += corte_bajista_adx
             datos_operaciones["ganadora?"] += ganada_perdida
+            pd.DataFrame.to_csv(datos_operaciones, "datos_operaciones.csv")
             live_data.clear()
             rango_precios.clear()
-        except:
-            print("hubo error, verificar si la ejecucion continua")
 
 
 
@@ -260,6 +273,33 @@ if __name__ == "__main__":
     hora = input("introduzca la hora de inicio (militar): ")
     minuto = input("introduzca el minuto de inicio: ")
     tiempo = int(input("introduzca el tiempo de ejecucion en minutos: "))
+    numero_noticias = int(input("Introduzca el numero de noticias: "))
+    noticia1 = 0
+    noticia2 = 0
+    noticia3 = 0
+    if numero_noticias == 0:
+        pass
+    elif numero_noticias == 1:
+        hora_noticia = input("Introduzca la hora de la noticia 30 minutos antes")
+        minuto_noticia = input("Introduzca el minuto de la noticia 30 minutos antes")
+        noticia1 = f'2020-{mes}-{dia} {hora_noticia}:{minuto_noticia}'
+    elif numero_noticias == 2:
+        hora_noticia1 = input("Introduzca la hora de la primera noticia 30 minutos antes")
+        minuto_noticia1 = input("Introduzca el minuto de la primera noticia 30 minutos antes")
+        noticia1 = f'2020-{mes}-{dia} {hora_noticia1}:{minuto_noticia1}'
+        hora_noticia2 = input("Introduzca la hora de la segunda noticia 30 minutos antes")
+        minuto_noticia2 = input("Introduzca el minuto de la segunda noticia 30 minutos antes")
+        noticia2 = f'2020-{mes}-{dia} {hora_noticia2}:{minuto_noticia2}'
+    elif numero_noticias == 3:
+        hora_noticia1 = input("Introduzca la hora de la primera noticia 30 minutos antes")
+        minuto_noticia1 = input("Introduzca el minuto de la noticia 30 minutos antes")
+        noticia1 = f'2020-{mes}-{dia} {hora_noticia1}:{minuto_noticia1}'
+        hora_noticia2 = input("Introduzca la hora de la segunda noticia 30 minutos antes")
+        minuto_noticia2 = input("Introduzca el minuto de la segunda noticia 30 minutos antes")
+        noticia2 = f'2020-{mes}-{dia} {hora_noticia2}:{minuto_noticia2}'
+        hora_noticia3 = input("Introduzca la hora de la tercera noticia 30 minutos antes")
+        minuto_noticia3 = input("Introduzca el minuto de la tercera noticia 30 minutos antes")
+        noticia3 = f'2020-{mes}-{dia} {hora_noticia1}:{minuto_noticia1}'
     while time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) != f'2020-{mes}-{dia} {hora}:{minuto}:00':
         pass
-    run(tiempo, primera_divisa, segunda_divisa)
+    run(tiempo, primera_divisa, segunda_divisa, numero_noticias, (noticia1, noticia2, noticia3))
